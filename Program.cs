6362d8c9a8f9be5431e918f90307b5c28476a79b
Steve Driscoll <steved@intuitivesystems.co.uk>
2020-08-27 09:01:23 +0100

    3path example generated + notes

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace PuzzleGraphGenerator
     {
         static void Main()
         {
-            var graph = Create2PathGraph(); 
+            var graph = Create3PathGraph(); 
             var serializer = new XmlSerializer(typeof(GraphContainer));
 
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
@@ -98,6 +98,59 @@ namespace PuzzleGraphGenerator
             graph.AddNode(id, label).AddGraphics(0, starty + (stepy * (max_nodes + 2))).AddLabelGraphics(label);
             graph.AddEdge(joinids[0], id).AddEdgeGraphics();
             graph.AddEdge(joinids[1], id).AddEdgeGraphics();
+            previd = id++;
+
+            graph.AddNode(id, "Finish").AddGraphics(0, starty + (stepy * (max_nodes + 3))).AddLabelGraphics("Finish");
+            graph.AddEdge(previd, id).AddEdgeGraphics();
+
+            return container;
+        }
+
+        private static GraphContainer Create3PathGraph()
+        {
+            var container = GraphContainer.CreateGraphContainer();
+            var graph = container.AddGraph();
+
+            const int max_nodes = 3;
+            const int offsetx = 150;
+            const int stepy = 80;
+            const int chara = 65;
+            var starty = -220;
+
+            graph.AddNode(0, "Start").AddGraphics(0, starty).AddLabelGraphics("Start");
+            graph.AddNode(1, "Puzzle 1").AddGraphics(0, starty + stepy).AddLabelGraphics("Puzzle 1");
+
+            graph.AddEdge(0, 1).AddEdgeGraphics();
+
+            var label = string.Empty;
+            var id = 2;
+            var previd = 1;
+            var joinids = new int[3];
+
+            for (int i = -1; i <= 1; i++)
+            {
+                previd = 1;
+
+                for (int j = 2; j < 2 + max_nodes; j++)
+                {
+                    var prefix = (char)(chara + (i + 1));
+                    var y = starty + (stepy * j);
+                    label = $"Puzzle {prefix}{j}";
+
+                    graph.AddNode(id, label).AddGraphics(i * offsetx, y).AddLabelGraphics(label);
+                    graph.AddEdge(previd, id).AddEdgeGraphics();
+                    previd = id;
+                    id++;
+                }
+
+                joinids[(i + 1)] = previd;
+            }
+
+            label = $"Puzzle {max_nodes + 2}";
+            graph.AddNode(id, label).AddGraphics(0, starty + (stepy * (max_nodes + 2))).AddLabelGraphics(label);
+            graph.AddEdge(joinids[0], id).AddEdgeGraphics();
+            graph.AddEdge(joinids[1], id).AddEdgeGraphics();
+            graph.AddEdge(joinids[2], id).AddEdgeGraphics();
             previd = id++;
 
             graph.AddNode(id, "Finish").AddGraphics(0, starty + (stepy * (max_nodes + 3))).AddLabelGraphics("Finish");
